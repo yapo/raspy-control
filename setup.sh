35802938514063734c73be963308f566b7b7a39c
Ppamo <pablo@ppamo.cl>
2014-06-30 04:35:36 +0000

    complete service

--- setup.sh
+++ setup.sh
@@ -9,16 +9,15 @@ fi
 
 # cleanup
 rm -f /etc/init.d/raspy-control.sh
+rm -f /etc/rc0.d/*raspy-control
+rm -f /etc/rc1.d/*raspy-control
 rm -f /etc/rc2.d/*raspy-control
 rm -f /etc/rc3.d/*raspy-control
+rm -f /etc/rc4.d/*raspy-control
+rm -f /etc/rc5.d/*raspy-control
+rm -f /etc/rc6.d/*raspy-control
 
 # install the raspy-control scripts
 chmod +x raspy-control.sh
 cp raspy-control.sh /etc/init.d/raspy-control.sh
-cd /etc/rc2.d
-ln -s ../init.d/raspy-control.sh S03raspy-control
-ln -s ../init.d/raspy-control.sh K03raspy-control
-cd /etc/rc3.d
-ln -s ../init.d/raspy-control.sh S03raspy-control
-ln -s ../init.d/raspy-control.sh K03raspy-control
-
+update-rc.d raspy-control.sh defaults
